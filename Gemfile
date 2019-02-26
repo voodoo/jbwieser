e34947cd4ee1027b23f1789c9e6603394d365b22
Paul Vudmaska <paul@vudmaska.com>
2019-02-26 13:05:07 -0600

    add gh-pages

--- Gemfile
+++ Gemfile
@@ -1,6 +1,9 @@
 source 'https://rubygems.org'
 
 gem 'middleman', '~> 4.2'
+gem 'middleman-gh-pages'
+
+
 #gem 'middleman-autoprefixer', '~> 2.7'
 
 # gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
--- config.rb
+++ config.rb
@@ -17,6 +17,13 @@ page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
+
+activate :deploy do |deploy|
+    deploy.method = :git
+    deploy.branch = 'gh-pages'
+    deploy.build_before = true
+end
+
 # With alternative layout
 # page '/path/to/file.html', layout: 'other_layout'
 
